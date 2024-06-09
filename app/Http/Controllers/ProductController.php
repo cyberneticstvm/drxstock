afa8ad0d37397c7c4136eb04808504da624be520
Cybernetics <mail@cybernetics.me>
2024-06-09 14:30:11 +0530

    small updates

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -115,12 +115,10 @@ class ProductController extends Controller
             })->when($request->sph == 0 && $request->cyl != 0, function ($q) use ($cyl, $request) {
                 return $q->whereIn('cyl', $cyl)->whereNull('sph')->orWhere('sph', 0);
             })->when($request->sph != 0 && $request->cyl != 0, function ($q) use ($sph, $cyl, $request) {
-                return $q->whereIn('sph', $sph)->orWhereIn('cyl', $cyl)->when($request->coating_id, function ($q) use ($request) {
-                    return $q->where('coating_id', $request->coating_id);
-                });
+                return $q->whereIn('sph', $sph)->orWhereIn('cyl', $cyl);
             })->when($request->eye != '', function ($q) use ($request) {
-                return $q->where('coating_id', $request->coating_id)->where('type_id', $request->type_id)->where('material_id', $request->material_id)->where('eye', $request->eye);
-            })->orderByDesc('add')->get();
+                return $q->where('eye', $request->eye);
+            })->where('coating_id', $request->coating_id)->where('type_id', $request->type_id)->where('material_id', $request->material_id)->orderByDesc('add')->get();
 
             /*return $q->whereRaw("IF($spherical, CAST($spherical AS DECIMAL(4,2)) = CAST(sph AS DECIMAL(4,2))+CAST(cyl AS DECIMAL(4,2)), 1)")->whereRaw("IF($cylinder, CAST($cylinder AS DECIMAL(4,2)) = CAST(0-cyl AS DECIMAL(4,2)), 1)")->orWhereRaw("sph=$spherical AND cyl=$cylinder");*/
         } catch (Exception $e) {
