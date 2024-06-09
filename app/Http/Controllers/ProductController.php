f02cdbd4de2ca3e002b6a23f7676235220482d64
Cybernetics <mail@cybernetics.me>
2024-06-09 14:13:17 +0530

    small updates

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -87,8 +87,6 @@ class ProductController extends Controller
         $cylinder = $request->cyl;
         $sph = [$request->sph, number_format($request->sph, 2), number_format($request->sph + $request->cyl, 2)];
         $cyl = [$request->cyl, number_format(0 - $request->cyl, 2)];
-        dd($cyl);
-        die;
         $add = [number_format($request->add, 2), number_format($request->add + 0.25, 2), number_format($request->add - 0.25, 2)];
         $powers = Power::all();
         $types = Type::all();
@@ -117,7 +115,7 @@ class ProductController extends Controller
             })->when($request->sph == 0 && $request->cyl != 0, function ($q) use ($cyl) {
                 return $q->whereIn('cyl', $cyl)->whereNull('sph')->orWhere('sph', 0);
             })->when($request->sph != 0 && $request->cyl != 0, function ($q) use ($sph, $cyl) {
-                return $q->whereIn('sph', $sph)->whereIn('cyl', $cyl);
+                return $q->whereIn('sph', $sph)->orWhereIn('cyl', $cyl);
                 /*return $q->whereRaw("IF($spherical, CAST($spherical AS DECIMAL(4,2)) = CAST(sph AS DECIMAL(4,2))+CAST(cyl AS DECIMAL(4,2)), 1)")->whereRaw("IF($cylinder, CAST($cylinder AS DECIMAL(4,2)) = CAST(0-cyl AS DECIMAL(4,2)), 1)")->orWhereRaw("sph=$spherical AND cyl=$cylinder");*/
             })->when($request->eye != '', function ($q) use ($request) {
                 return $q->where('eye', $request->eye);
