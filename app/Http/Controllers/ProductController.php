69479107f9b23c01f17ad10f7be2592286248499
Cybernetics <mail@cybernetics.me>
2024-06-09 13:41:03 +0530

    small updates

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -111,7 +111,7 @@ class ProductController extends Controller
             })->when($request->add != '', function ($q) use ($add) {
                 return $q->whereIn('add', $add);
             })->when($request->sph != '' && $request->cyl != '', function ($q) use ($spherical, $cylinder) {
-                return $q->where('sph', $spherical)->orWhere('sph', "CAST($spherical AS DECIMAL(4,2)) = CAST(sph AS DECIMAL(4,2))+CAST(cyl AS DECIMAL(4,2))")->where('cyl', $cylinder);
+                return $q->where('sph', $spherical)->orWhere('sph', "CAST($spherical AS DECIMAL(4,2)) = CAST(sph AS DECIMAL(4,2))+CAST(cyl AS DECIMAL(4,2))")->where('cyl', $cylinder)->orWhere('cyl', "CAST($cylinder AS DECIMAL(4,2)) = CAST(0-cyl AS DECIMAL(4,2))");
                 /*return $q->whereRaw("IF($spherical, CAST($spherical AS DECIMAL(4,2)) = CAST(sph AS DECIMAL(4,2))+CAST(cyl AS DECIMAL(4,2)), 1)")->whereRaw("IF($cylinder, CAST($cylinder AS DECIMAL(4,2)) = CAST(0-cyl AS DECIMAL(4,2)), 1)")->orWhereRaw("sph=$spherical AND cyl=$cylinder");*/
             })->when($request->sph != '' && $request->cyl == '', function ($q) use ($sph) {
                 return $q->whereIn('sph', $sph)->whereNull('cyl')->orwhere('cyl', 0);
