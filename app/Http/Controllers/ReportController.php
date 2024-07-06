b7f1f2b7ffbc12a61f827e556a666a626dcbb1c1
Cybernetics <mail@cybernetics.me>
2024-07-06 10:39:38 +0530

    small updates

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -17,7 +17,7 @@ class ReportController extends Controller implements HasMiddleware
 
     public function __construct()
     {
-        $this->branches = Sales::select('branch')->get()->unique()->pluck('name', 'name');
+        $this->branches = Sales::all()->unique('name')->pluck('name', 'name');
     }
     public static function middleware(): array
     {
